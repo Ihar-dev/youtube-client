7177e2833d29cd2ed7470194b7dea351dcbfec5f
Ihar Kozik <ihar.kozik@gmail.com>
2022-03-24 21:10:35 +0300

    feat: make components

--- YouTube-client-app/src/app/app.module.ts
+++ YouTube-client-app/src/app/app.module.ts
@@ -3,10 +3,16 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { SearchResultsComponent } from './components/search/search-results/search-results.component';
+import { SearchItemComponent } from './components/search/search-item/search-item.component';
+import { HeaderComponent } from './components/header/header/header.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    SearchResultsComponent,
+    SearchItemComponent,
+    HeaderComponent
   ],
   imports: [
     BrowserModule,
