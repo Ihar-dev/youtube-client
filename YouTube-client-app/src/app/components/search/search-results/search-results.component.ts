b353d24255eea2fbfe7c95a41798729b0c4a01b8
Ihar Kozik <ihar.kozik@gmail.com>
2022-04-02 18:53:35 +0300

    feat: add getSearchResults method

--- YouTube-client-app/src/app/components/search/search-results/search-results.component.ts
+++ YouTube-client-app/src/app/components/search/search-results/search-results.component.ts
@@ -1,24 +1,53 @@
 import {
   Input,
   Component,
-  OnInit,
   OnChanges,
 } from '@angular/core';
+
+import {
+  SearchResponse,
+} from '../../../models/search-response.model';
 
 @Component({
   selector: 'app-search-results',
   templateUrl: './search-results.component.html',
   styleUrls: ['./search-results.component.scss'],
 })
-export class SearchResultsComponent implements OnInit, OnChanges {
+export class SearchResultsComponent implements OnChanges {
   @Input() public dataForSearch: string;
   @Input() public SortingBarView: boolean;
 
-  ngOnChanges(): void {
-    if (this.dataForSearch) {
-      console.log(this.dataForSearch);
-    }
+  private async handleSearch(): Promise < void > {
+    let searchData: SearchResponse;
+    searchData = this.getDefaultSearchData();
+    searchData = await this.getSearchResults();
+    console.log(this.dataForSearch);
+    console.log(searchData);
   }
 
-  ngOnInit(): void {}
+  private async getSearchResults(): Promise < SearchResponse > {
+    let searchData: SearchResponse;
+    searchData = this.getDefaultSearchData();
+    const url = 'https://raw.githubusercontent.com/Ihar-dev/tasks/master/tasks/angular/response.json';
+    const res = await fetch(url);
+    searchData = await res.json();
+    return searchData;
+  }
+
+  private getDefaultSearchData(): SearchResponse {
+    const data = {
+      kind: '',
+      etag: '',
+      pageInfo: {
+        totalResults: 0,
+        resultsPerPage: 0,
+      },
+      items: [],
+    };
+    return data;
+  }
+
+  ngOnChanges(): void {
+    if (this.dataForSearch) this.handleSearch();
+  }
 }
