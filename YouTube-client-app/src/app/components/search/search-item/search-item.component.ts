2fdb980bd8e40846d694d9538561defd93fccf3f
Ihar Kozik <ihar.kozik@gmail.com>
2022-04-04 16:30:56 +0300

    feat: add getBorderColor method

--- YouTube-client-app/src/app/components/search/search-item/search-item.component.ts
+++ YouTube-client-app/src/app/components/search/search-item/search-item.component.ts
@@ -1,18 +1,37 @@
 import {
   Input,
   Component,
-  OnInit,
 } from '@angular/core';
 
-import { SearchItem } from '../../../models/search-item.model';
+import {
+  SearchItem,
+} from '../../../models/search-item.model';
 
+enum Settings {
+  sevenDaysDuration = 1000 * 60 * 60 * 24 * 7,
+}
 @Component({
   selector: 'app-search-item',
   templateUrl: './search-item.component.html',
   styleUrls: ['./search-item.component.scss'],
 })
-export class SearchItemComponent implements OnInit {
+export class SearchItemComponent {
   @Input() item: SearchItem;
 
-  ngOnInit(): void {}
+  public getBorderColor(): string {
+    const dateNow = new Date();
+    const itemDate = new Date(this.item.snippet.publishedAt);
+    if (Number(dateNow) - Number(itemDate) < Settings.sevenDaysDuration) return 'blue';
+    const monthDifference = (dateNow.getUTCFullYear() * 12 + dateNow.getUTCMonth())
+    - (itemDate.getUTCFullYear() * 12 + itemDate.getUTCMonth());
+    if (monthDifference <= 1) {
+      if (!monthDifference) return 'green';
+      if (dateNow.getUTCDate() < itemDate.getUTCDate()) return 'green';
+    }
+    if (monthDifference <= 6) {
+      if (monthDifference < 6) return 'yellow';
+      if (dateNow.getUTCDate() < itemDate.getUTCDate()) return 'yellow';
+    }
+    return 'red';
+  }
 }
