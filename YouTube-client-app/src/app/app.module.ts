1c63fc0b0256ba7192883e8789202cba347f41cd
Ihar Kozik <ihar.kozik@gmail.com>
2022-04-07 19:24:17 +0300

    feat: create modules

--- YouTube-client-app/src/app/app.module.ts
+++ YouTube-client-app/src/app/app.module.ts
@@ -2,6 +2,7 @@ import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { BrowserModule } from '@angular/platform-browser';
 
+import { CoreModule } from './core/core.module';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { SearchResultsComponent } from './components/search/search-results/search-results.component';
@@ -21,6 +22,7 @@ import { SortingBarComponent } from './components/header/sorting-bar/sorting-bar
     BrowserModule,
     AppRoutingModule,
     FormsModule,
+    CoreModule,
   ],
   providers: [],
   bootstrap: [AppComponent],
