d21eb587d7d4e2f9259384a2e06d88a8fe785665
Ihar Kozik <ihar.kozik@gmail.com>
2022-04-09 17:41:20 +0300

    refactor: move components

--- YouTube-client-app/src/app/app-routing.module.ts
+++ YouTube-client-app/src/app/app-routing.module.ts
@@ -1,7 +1,13 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+import { SearchResultsComponent } from './youtube/components/search/search-results/search-results.component';
+
+const routes: Routes = [
+  { path: 'dist/you-tube-client-app', component: SearchResultsComponent },
+  /* { path: 'about', component: AboutComponent},
+  { path: '**', component: NotFoundComponent } */
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- YouTube-client-app/src/app/app.module.ts
+++ YouTube-client-app/src/app/app.module.ts
@@ -4,11 +4,12 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { CoreModule } from './core/core.module';
 import { AppRoutingModule } from './app-routing.module';
+
 import { AppComponent } from './app.component';
-import { SearchResultsComponent } from './components/search/search-results/search-results.component';
-import { SearchItemComponent } from './components/search/search-item/search-item.component';
-import { HeaderComponent } from './components/header/header/header.component';
-import { SortingBarComponent } from './components/header/sorting-bar/sorting-bar.component';
+import { SearchResultsComponent } from './youtube/components/search/search-results/search-results.component';
+import { SearchItemComponent } from './youtube/components/search/search-item/search-item.component';
+import { HeaderComponent } from './core/components/header/header.component';
+import { SortingBarComponent } from './core/components/sorting-bar/sorting-bar.component';
 
 @NgModule({
   declarations: [
