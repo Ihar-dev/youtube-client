90386ce1d6e110caf82a6d136110b11d0eee7a13
Ihar Kozik <ihar.kozik@gmail.com>
2022-04-11 01:53:32 +0300

    feat: make login container

--- YouTube-client-app/src/app/material/material.module.ts
+++ YouTube-client-app/src/app/material/material.module.ts
@@ -1,10 +1,12 @@
 import { NgModule } from '@angular/core';
 import { MatSliderModule } from '@angular/material/slider';
 import { MatInputModule } from '@angular/material/input';
+import { MatFormFieldModule } from '@angular/material/form-field';
 
 const MaterialComponents = [
   MatSliderModule,
   MatInputModule,
+  MatFormFieldModule,
 ];
 
 @NgModule({
