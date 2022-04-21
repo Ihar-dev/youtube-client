c929226b3bd69ae5e109edae0f59e4cff605b3f8
Ihar Kozik <ihar.kozik@gmail.com>
2022-04-21 22:24:31 +0300

    feat: add login validation

--- YouTube-client-app/src/app/auth/pages/login/login.component.ts
+++ YouTube-client-app/src/app/auth/pages/login/login.component.ts
@@ -1,12 +1,19 @@
-import { Component, OnInit } from '@angular/core';
-import { Router } from '@angular/router';
+import {
+  Component,
+  OnInit,
+} from '@angular/core';
+import {
+  Router,
+} from '@angular/router';
 import {
   FormGroup,
   FormControl,
   Validators,
 } from '@angular/forms';
 
-import { LoginService } from '../../services/login.service';
+import {
+  LoginService,
+} from '../../services/login.service';
 
 @Component({
   selector: 'app-login',
@@ -16,13 +23,16 @@ import { LoginService } from '../../services/login.service';
 
 export class LoginComponent implements OnInit {
   public readonly loginService: LoginService;
-  public readonly myForm : FormGroup;
+  public readonly myForm: FormGroup;
 
   constructor(loginService: LoginService, private router: Router) {
     this.loginService = loginService;
     this.myForm = new FormGroup({
-      userName: new FormControl('', Validators.minLength(4)),
-      userPassword: new FormControl('', Validators.minLength(6)),
+      userName: new FormControl('', Validators.email),
+      userPassword: new FormControl('', [
+        Validators.minLength(8),
+        Validators.pattern(/(?=.*[0-9])(?=.*[!@#$%^&*])(?=.*[a-z])(?=.*[A-Z])[0-9a-zA-Z!@#$%^&*]/),
+      ]),
     });
   }
 
