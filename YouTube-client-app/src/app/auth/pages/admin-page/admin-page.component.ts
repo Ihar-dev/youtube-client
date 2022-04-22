bad4704e4389ea17ac02b752cb23dcc0788073e4
Ihar Kozik <ihar.kozik@gmail.com>
2022-04-22 17:19:11 +0300

    feat: add inputs

--- YouTube-client-app/src/app/auth/pages/admin-page/admin-page.component.ts
+++ YouTube-client-app/src/app/auth/pages/admin-page/admin-page.component.ts
@@ -33,14 +33,33 @@ export class AdminPageComponent implements OnInit {
         Validators.minLength(3),
         Validators.maxLength(20),
       ]),
-      userPassword: new FormControl('', [
-        Validators.minLength(8),
-        Validators.pattern(/(?=.*[0-9])(?=.*[!@#$%^&*])(?=.*[a-z])(?=.*[A-Z])/),
+      userDescription: new FormControl('', [
+        Validators.maxLength(255),
+      ]),
+      userImg: new FormControl('', [
+        Validators.pattern(/(http|https):\/\/([\w.]+\/?)\S*/),
+      ]),
+      userVideo: new FormControl('', [
+        Validators.pattern(/(http|https):\/\/([\w.]+\/?)\S*/),
       ]),
     });
   }
 
   public get userTitle(): AbstractControl | null {
     return this.cardForm.get('userTitle');
+  }
+
+  public submitForm(
+    userTitle: string,
+    isUserTitleInvalid: boolean,
+    userDescription: string,
+    isUserDescriptionInvalid: boolean,
+    userImg: string,
+    isUserImgInvalid: boolean,
+    userVideo: string,
+    isUserVideoInvalid: boolean,
+  ): void {
+    if (isUserTitleInvalid || isUserDescriptionInvalid || isUserImgInvalid || isUserVideoInvalid) return;
+    console.log(`submitting: title- ${userTitle}, description- ${userDescription}, img- ${userImg}, video- ${userVideo}`);
   }
 }
