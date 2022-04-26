c86f250256a3db479a5b68c1aa521b505f818e09
Ihar Kozik <ihar.kozik@gmail.com>
2022-04-26 18:36:08 +0300

    feat: edit auth routing

--- YouTube-client-app/src/app/auth/auth-routing.module.ts
+++ YouTube-client-app/src/app/auth/auth-routing.module.ts
@@ -1,12 +1,13 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
+import { MainGuard } from '../youtube/guards/main.guard';
 import { LoginComponent } from './pages/login/login.component';
 import { AdminPageComponent } from './pages/admin-page/admin-page.component';
 
 const authRoutes: Routes = [
   { path: 'login', component: LoginComponent },
-  { path: 'admin', component: AdminPageComponent },
+  { path: 'admin', component: AdminPageComponent, canActivate: [MainGuard] },
 ];
 
 @NgModule({
