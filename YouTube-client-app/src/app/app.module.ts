1ff7695600654933ea7eb022370ebb4646aded2b
Ihar Kozik <ihar.kozik@gmail.com>
2022-04-26 22:50:20 +0300

    feat: add NGRX

--- YouTube-client-app/src/app/app.module.ts
+++ YouTube-client-app/src/app/app.module.ts
@@ -9,6 +9,12 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './core/components/header/header.component';
 import { SortingBarComponent } from './core/components/sorting-bar/sorting-bar.component';
+import { StoreModule } from '@ngrx/store';
+import { reducers, metaReducers } from './redux/reducers';
+import { StoreDevtoolsModule } from '@ngrx/store-devtools';
+import { environment } from '../environments/environment';
+import { EffectsModule } from '@ngrx/effects';
+import { AppEffects } from './redux/effects/app.effects';
 
 @NgModule({
   declarations: [
@@ -22,6 +28,11 @@ import { SortingBarComponent } from './core/components/sorting-bar/sorting-bar.c
     BrowserAnimationsModule,
     CoreModule,
     AppRoutingModule,
+    StoreModule.forRoot(reducers, {
+      metaReducers
+    }),
+    StoreDevtoolsModule.instrument({ maxAge: 25, logOnly: environment.production }),
+    EffectsModule.forRoot([AppEffects]),
   ],
   providers: [],
   bootstrap: [AppComponent],
